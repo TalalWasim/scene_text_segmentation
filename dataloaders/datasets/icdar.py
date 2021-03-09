a1cab874c8b1f5c0335691346c0b02c8b1530069
TalalWasim <wasimtalal@gmail.com>
2021-03-09 07:50:44 +0100

    updated naming of gt images

--- dataloaders/datasets/icdar.py
+++ dataloaders/datasets/icdar.py
@@ -53,7 +53,7 @@ class ICDARSegmentation(Dataset):
             for ii, line in enumerate(lines):
                 _image = os.path.join(self._image_dir, line + ".jpg") #".png")
                 #print("img is: {}".format(_image))
-                _cat = os.path.join(self._cat_dir, line + ".png")
+                _cat = os.path.join(self._cat_dir, line + "_GT.png")
                 #print("gt is: {}".format(_cat))
                 assert os.path.isfile(_image)
                 assert os.path.isfile(_cat)
